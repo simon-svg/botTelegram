7fe94aeb67f07e42f2ea1f45330e223e93a2f541
Simon <63739585+simon-svg@users.noreply.github.com>
2020-09-25 18:03:54 +0400

    final commit

---
+++ build/precache-manifest.241bca3e0b95b3957f300293963b20aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d30426114ffef80cb868822a169a1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4b53467a77316e026b2",
+    "url": "/static/css/main.4d82b4e2.chunk.css"
+  },
+  {
+    "revision": "1f3ea141e5c3d19b2a5b",
+    "url": "/static/js/2.68385335.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.68385335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4b53467a77316e026b2",
+    "url": "/static/js/main.f4d6a5b2.chunk.js"
+  },
+  {
+    "revision": "b2b191981a6b381042f2",
+    "url": "/static/js/runtime-main.5a587cd2.js"
+  }
+]);
